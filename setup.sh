4b262197701ecdf885b3be35c8491473696ddc65
Can Demircan <cdemircan@tutanota.com>
2025-07-12 11:09:09 +0200

    fix setup script

--- setup.sh
+++ setup.sh
@@ -3,11 +3,9 @@
 # Check if uv is installed
 if command -v uv >/dev/null 2>&1; then
     echo "uv is already installed."
-    exit 0
-fi
 
 # If uv is not installed, try to install it
-if command -v curl >/dev/null 2>&1; then
+elif command -v curl >/dev/null 2>&1; then
     echo "installing uv using curl..."
     curl -LsSf https://astral.sh/uv/install.sh | sh
 elif command -v wget >/dev/null 2>&1; then
