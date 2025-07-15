56f650179f1d5d5a49bb2291ed7e4872c8855737
Can Demircan <cdemircan@tutanota.com>
2025-07-15 09:54:35 +0200

    hello uv lock

--- setup.sh
+++ setup.sh
@@ -12,29 +12,15 @@ elif command -v wget >/dev/null 2>&1; then
     echo "installing uv using wget..."
     wget -qO- https://astral.sh/uv/install.sh | sh
 else
-    echo "error: neither curl nor wget is installed. please install one to continue or install uv yourself before using this template." >&2
+    echo "error: neither curl nor wget is installed. please install one to continue or install uv yourself before carrying on" >&2
     exit 1
 fi
 
 export UV_TORCH_BACKEND=auto
-
-# Check if --disposable flag is provided
-if [[ "$1" == "--disposable" ]]; then
-    export TMPDIR=/localscratch/
-    cd /localscratch/
-    uv venv -p 3.12.5 .metarep
-    source .metarep/bin/activate
-    cd /lustre/groups/hcai/workspace/can.demircan/metarep
-else
-    uv venv -p 3.12.5  .venv
-    source .venv/bin/activate
-fi
-
-uv pip install -e '.[dev]'
-uv pip install --no-build-isolation '.[linux]'
+uv sync --all-extras
+source .venv/bin/activate
 pre-commit install
 pre-commit run --all-files
 
-# add example* to .gitignore
-echo "example*" >> .gitignore
 
+echo -e "\nexample*" >> .gitignore
