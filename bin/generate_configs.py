25aed5f105f6e7595c5dba6a586e1412d261187b
Can Demircan <cdemircan@tutanota.com>
2025-08-24 19:18:03 +0200

    config update

--- bin/generate_configs.py
+++ bin/generate_configs.py
@@ -29,7 +29,7 @@ def main(output_dir: Path = Path("data/configs")):
     }
 
     param_grid = {
-        "lr": [0.0001 ,0.00025, 0.0005, 0.001, 0.0025], 
+        "lr": [0.0001 ,0.00025, 0.0005, 0.001], 
         "model_name": ["dinov3-vitb16-pretrain-lvd1689m", "CLIP-ViT-B-32-DataComp.XL-s13B-b90K_sae-top_k-64-cls_only-layer_11-hook_resid_post_raw",  "siglip2-base-patch16-224"]
     }
 
